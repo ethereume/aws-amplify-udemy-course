46ae5c17af0354b28b53174d417e69106ca4ceda
m.zawadzki <m.zawadzki@m4b.pl>
2019-03-08 16:15:14 +0100

    Init App

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Amplify from "aws-amplify";
+import awsmobile from "./aws-exports";
+Amplify.configure(awsmobile)
+
+ReactDOM.render(<App />,document.getElementById('root'));
